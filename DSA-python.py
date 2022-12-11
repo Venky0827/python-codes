e21988de1c675a98ef71a388006da70b9ad84259
Venky0827 <112114447+Venky0827@users.noreply.github.com>
2022-12-11 18:25:14 +0530

    Update DSA-python.py

--- DSA-python.py
+++ DSA-python.py
@@ -41,3 +41,36 @@ class Queue(object):
         return len(self.items)
 
 q = Queue()
+
+#---------------------- Deque ---------------------
+
+class Deque(object):
+
+    def __init__(self):
+        self.items = []
+
+    def IsEmpty(self):
+        return self.items == []
+
+    def addFront(self,item):
+        self.items.append(item)
+
+    def addRear(self,item):
+        self.items.insert(0,item)
+
+    def removeFront(self):
+        self.items.pop()
+
+    def removeReart(self):
+        self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+    
+    def display(self):
+        return self.items
+
+d = Deque()
+
+
+
